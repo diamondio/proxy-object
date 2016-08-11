59cce349c7b6998d5a0bc4f900cc636a4b0c8564
Jake Nielsen <jake.k.nielsen@gmail.com>
2016-08-11 15:17:04 -0700

    Hurray

---
+++ index.js
@@ -0,0 +1,50 @@
+var request = require('request');
+
+var callMethod = function (url, getHeaders, method, args, cb) {
+  request({
+    url: url,
+    dataType: 'json',
+    method: "POST",
+    headers: getHeaders(),
+    json: {
+      args,
+      method
+    }
+  }, function (err, res) {
+    if (err && !res) return cb({ message: 'no_internet', details: err });
+    if (!res) return cb({ message: 'no_response' });
+    if (err) return cb(err);
+    if (res.statusCode !== 200) return cb({ message: 'unexpected_status_code', details: `${res.statusCode} ${res.body}`});
+
+    cb.apply(null, res.body);
+  });
+}
+
+exports.createProxy = function (url, getHeaders, methodList) {
+  var proxyObject = {};
+  methodList.forEach(function (methodName) {
+    proxyObject[methodName] = function () {
+      var callback = () => {};
+      if (arguments.length) {
+        callback = arguments[arguments.length - 1];
+      }
+      callMethod(url, getHeaders, methodName, Array.prototype.slice.call(arguments).slice(0, arguments.length - 1), callback);
+    }
+  })
+  return proxyObject;
+}
+
+exports.createMiddleware = function (obj) { 
+  return function (req, res, next) {
+    var args = req.body['args'];
+    var method = req.body['method'];
+    if (!args) return res.status(400).json({message: 'Missing args'});
+    if (!method) return res.status(400).json({message: 'Missing method'});
+    if (!obj[method]) return res.status(400).json({message: 'Proxy object does not have method: ' + method});
+    args.push(function () {
+      res.json(Array.prototype.slice.call(arguments));
+    });
+    obj[method].apply(null, args);
+  };
+};
+
---
+++ test/simple.js
@@ -0,0 +1,98 @@
+var assert     = require('assert');
+var bodyParser = require('body-parser');
+var express    = require('express');
+
+var proxyObject = require('../index');
+
+describe('Simple Objects', function () {
+  var server = null;
+
+  beforeEach(function (done){
+    server = null;
+    done();
+  });
+
+  afterEach(function (done) {
+    if (server) server.close();
+    done();
+  });
+
+  var emptyHeaders = function () {
+    return {};
+  }
+
+  it('One method object', function (done) {
+    var app = express();
+    app.use(bodyParser.json());
+
+    var simpleObject = {
+      'aMethod': function (cb) {
+        setImmediate(function () { cb(null, 'Hello!') });
+      }
+    }
+
+    var middleware = proxyObject.createMiddleware(simpleObject);
+    app.post('/simple', proxyObject.createMiddleware(simpleObject));
+
+    server = app.listen(3101, function () {
+      var proxiedObject = proxyObject.createProxy('http://localhost:3101/simple', emptyHeaders, ['aMethod']);
+      proxiedObject.aMethod(function (err, result) {
+        assert.ok(!err);
+        assert.equal(result, 'Hello!');
+        done();
+      })
+    });
+  });
+
+  it('One method object with arguments', function (done) {
+    var app = express();
+    app.use(bodyParser.json());
+
+    var simpleObject = {
+      'aMethod': function (anArg, cb) {
+        setImmediate(function () { cb(null, anArg) });
+      }
+    }
+
+    var middleware = proxyObject.createMiddleware(simpleObject);
+    app.post('/simple', proxyObject.createMiddleware(simpleObject));
+
+    server = app.listen(3101, function () {
+      var proxiedObject = proxyObject.createProxy('http://localhost:3101/simple', emptyHeaders, ['aMethod']);
+      proxiedObject.aMethod('AHOY!', function (err, result) {
+        assert.ok(!err);
+        assert.equal(result, 'AHOY!');
+        done();
+      })
+    });
+  });
+
+  it('Object with several methods', function (done) {
+    var app = express();
+    app.use(bodyParser.json());
+
+    var simpleObject = {
+      'm1': function (cb) {
+        setImmediate(function () { cb(null, 'Hello ') });
+      },
+      'm2': function (cb) {
+        setImmediate(function () { cb(null, 'World') });
+      },
+      'm3': function (cb) {
+        setImmediate(function () { cb(null, '!') });
+      }
+    }
+
+    var middleware = proxyObject.createMiddleware(simpleObject);
+    app.post('/simple', proxyObject.createMiddleware(simpleObject));
+
+    server = app.listen(3101, function () {
+      var proxiedObject = proxyObject.createProxy('http://localhost:3101/simple', emptyHeaders, ['m1', 'm2', 'm3']);
+      proxiedObject.m2(function (err, result) {
+        assert.ok(!err);
+        assert.equal(result, 'World');
+        done();
+      })
+    });
+  });
+});
